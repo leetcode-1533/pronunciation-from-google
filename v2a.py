ff1624e1f73394d1a50b902add954853943629cd
y1275963 <1275963@gmail.com>
2015-07-09 22:22:38 +0800

    Initial Commit

---
+++ v2a.py
@@ -0,0 +1,20 @@
+import urllib2
+import os
+
+
+class audio:
+    def __init__(self,word):
+        self.g_path = "https://ssl.gstatic.com/dictionary/static/sounds/de/0/"
+        self.mp3_path = self.g_path + word + ".mp3"
+        self.title = word
+        self.loc = "~/Desktop"
+        
+        self.mp3_download()
+        
+        
+    def mp3_download(self):
+        mp3 = urllib2.urlopen(self.mp3_path)
+        open(os.path.join(self.loc,self.title)+'.mp3','wb').write(mp3.read())
+
+if __name__ == "__main__":
+    audio("audio")
