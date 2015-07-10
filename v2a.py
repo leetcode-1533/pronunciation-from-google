0404212dfcb572ccbbf21f08db95679ce0011350
Yingjie <1275963@gmail.com>
2015-07-10 08:44:53 +0800

    Using the list name as folder name

--- v2a.py
+++ v2a.py
@@ -7,18 +7,22 @@ import urllib2
 import os
 
 class audio:
-    def __init__(self,word):
+    def __init__(self,foldername,word):
         
         self.title = word
-
+        
         self.g_path = "https://ssl.gstatic.com/dictionary/static/sounds/de/0/"
         self.mp3_path = self.g_path + word + ".mp3"
-        self.loc = "/Users/y1275963/Desktop/v2a/audio"
+        self.loc = os.path.join("/Users/y1275963/Desktop/v2a/audio",foldername)
         
+        if not os.path.exists(self.loc):
+            os.makedirs(self.loc)  
+            
         self.mp3_download()
         
         
     def mp3_download(self):
+        print self.mp3_path
         try:
             mp3 = urllib2.urlopen(self.mp3_path)
             open(os.path.join(self.loc,self.title)+'.mp3','wb').write(mp3.read())
@@ -30,10 +34,12 @@ if __name__ == "__main__":
     socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 1080)
     socket.socket = socks.socksocket
     
-    with open('/Users/y1275963/Desktop/v2a/list') as f:
+    foldername = 'tk1'
+    
+    with open(os.path.join('/Users/y1275963/Desktop/v2a',foldername)) as f:
         for line in f:
             aud = str(line)
-            audio(aud.strip())
+            audio(foldername,aud.strip())
     
     
 #    au = raw_input('vacubolary ')
