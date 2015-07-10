f2902a4e83dd5bed736074eab34965a00c6da2bc
Yingjie <1275963@gmail.com>
2015-07-10 15:20:36 +0800

    Folder path, Thread lock, Promote Input

--- v2a.py
+++ v2a.py
@@ -2,9 +2,14 @@
 
 import socks
 import socket
+import sys
 
 import urllib2
 import os
+import thread
+
+exerpath = '/Users/y1275963/Desktop/v2a'
+foldername = sys.argv[1]
 
 class audio:
     def __init__(self,foldername,word):
@@ -13,7 +18,7 @@ class audio:
         
         self.g_path = "https://ssl.gstatic.com/dictionary/static/sounds/de/0/"
         self.mp3_path = self.g_path + word + ".mp3"
-        self.loc = os.path.join("/Users/y1275963/Desktop/v2a/audio",foldername)
+        self.loc = os.path.join(exerpath,"audio",foldername)
         
         if not os.path.exists(self.loc):
             os.makedirs(self.loc)  
@@ -22,23 +27,26 @@ class audio:
         
         
     def mp3_download(self):
-        try:
-            mp3 = urllib2.urlopen(self.mp3_path)
-            open(os.path.join(self.loc,self.title)+'.mp3','wb').write(mp3.read())
-            print "Downloaded", self.title
-        except:
-            print "Wrong Word:", self.title   
+   #     try:
+        mp3 = urllib2.urlopen(self.mp3_path)
+        open(os.path.join(self.loc,self.title)+'.mp3','wb').write(mp3.read())
+        print "Downloaded", self.title
+    #    except:
+     #       print "Wrong Word:", self.title   
 
 if __name__ == "__main__":
     socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 1080)
     socket.socket = socks.socksocket
     
-    foldername = 'tk1'
+    lock = thread.allocate_lock()
     
     with open(os.path.join('/Users/y1275963/Desktop/v2a',foldername)) as f:
         for line in f:
+            lock.acquire()
             aud = str(line)
             audio(foldername,aud.strip())
+            lock.release()
+
     
     
 #    au = raw_input('vacubolary ')
