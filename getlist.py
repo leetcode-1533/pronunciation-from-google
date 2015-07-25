8d1b655f497931cd0b701a555841c525c50c3518
Yingjie <1275963@gmail.com>
2015-07-25 13:33:39 +0800

    Remove redundancy

--- getlist.py
+++ getlist.py
@@ -11,22 +11,40 @@ import sys
 import urllib
 from bs4 import BeautifulSoup
 
+def returnitem(cala):
+    for item in cala:
+        subitem = item.find('div',{'class':'text'})
+        yield subitem.text.encode('utf-8')
+
+def getdic(url):
+    
+    empdic = {}
+    html = urllib.urlopen(url).read()
+    soup = BeautifulSoup(html)
+    
+    lists_men = soup.findAll('div',{'class':'col_a col text'})
+    lists = soup.findAll('div',{'class':'col_b col text'})
+    
+    for item,meaning in zip(returnitem(lists),returnitem(lists_men)):
+        empdic[item] = meaning
+    
+    return empdic
+    
+        
+    
+    
+    
+
 
 if __name__ == "__main__":
     
     url = 'http://www.memrise.com/course/735854/gre-3000-10/'+str(sys.argv[1]).rstrip()+'/'
 
-    reload(sys)  
-    sys.setdefaultencoding('utf8')
+#    reload(sys)  
+#    sys.setdefaultencoding('utf8')
+    dic = getdic(url)
     
-    html = urllib.urlopen(url).read()
-    soup = BeautifulSoup(html)
-    
-    lists = soup.findAll('div',{'class':'col_a col text'})
-    
-    for item in lists:
-        subitem = item.find('div',{'class':'text'})
-        print str(subitem.text).lower()
+    print '\n'.join(dic.keys())
 
     
 
